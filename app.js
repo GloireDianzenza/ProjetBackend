e6cca734b7a0c98ad9d5464467eabd64c07b3233
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-07-12 16:48:50 +0200

    postman

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('I eat banabas!')
+})
+app.post("/",(req,res)=>{
+    res.send("EEE");
 })
 
 module.exports = app;
